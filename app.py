29f6c9f5ea09267e554d5af9b402b540667cf589
Roman Wins <Roman.Wins@rit.de>
2025-11-07 09:55:23 +0100

    favicon und Einbindung in index.html GETESTET

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from flask import send_from_directory
 from src import create_app
 
 app = create_app()
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
